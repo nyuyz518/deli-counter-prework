dd0b98055a8f6c92e264113a8889890179df979a
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 02:02:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,8 +17,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     message="The line is currently:"
-    katz_deli.each_with_index do |value, index|
-      message += " #{index.to_i+1}. #{value}"
+    katz_deli.each_with_index do |name, index|
+      message += " #{index.to_i+1}. #{name}"
     end
     puts "#{message}"
   end
