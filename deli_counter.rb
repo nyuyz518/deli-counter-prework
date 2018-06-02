0153becf9eee0f0473fcd0dc60b027a6bafff657
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 01:40:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ other_deli = ["Logan", "Avi", "Spencer"]
 
 def line (katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty."
+      puts "The line is currently empty."
    else 
       other_deli.each_with_index.collect do |index, name|
       puts "The line is currently: " + #{index} + #{name}
