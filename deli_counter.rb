07b44e0f995b872c49e2878580cb6214cecd94fc
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 01:52:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,9 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
       
+      
+def now_serving (katz_deli)
+  
+  katz_deli.unshift 
+  break if katz_deli.length == 0
+  puts 
