904ba64157cc18f5de3cb35d18f1e70a96b9b4fb
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 01:43:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,12 @@ def line (katz_deli)
       puts "#{index}. +  #{name}"
     end 
 end 
+
+def line(current_line)
+    if current_line.length == 0
+      puts "The line is currently empty."
+    else 
+      puts "The line is currently: "
+      current_line.each_with_index do |i, x| puts "#{i+1}. #{x}."
+    end 
+end
